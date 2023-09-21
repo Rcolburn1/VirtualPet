396db682c81d5823d41fa73271f41853d5191395
Nathan Colburn <nacolb@cs.csis.work>
2023-09-21 00:03:26 -0500

    wrote initial draft of Pikachu.h

--- Pikachu.h
+++ Pikachu.h
@@ -1,2 +1,20 @@
-//temp
-//
+#ifndef PIKACHU_H
+#define PIKACHU_H
+
+#include "Pokemon.h"
+
+class Pikachu : public Pokemon
+{
+	private:
+		int electricPower;
+
+	protected:
+		void increaseHappiness(int amount);
+
+	public:
+		Pikachu(const string& name, const string& species, int health, int power);
+		void useElectricShock();
+}
+
+#endif
+
